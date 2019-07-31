5a7ff4a3be064dcb151b930300486df1b97aab2b
bpames <bpames@ua.edu>
2019-07-31 13:29:35 -0500

    Fix normalize
    Modified normalize function to delete constant features.

--- normalize_test.m
+++ normalize_test.m
@@ -1,6 +1,21 @@
-function x=normalize_test(x, mu, sig)
-[n,~]=size(x);
+function x=normalize_test(x, mu, sig, feat)
+% NORMALIZE_TEST Transforms test data x according to training mean and
+% variance.
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% INPUT.
+% x - testing data.
+% mu - sample mean of training data.
+% sig - sample standard deviation of training data
+% feat - vector of indices of nonconstant training features.
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% OUTPUT.
+% x - transformed testing data.
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+% Delete nontrivial features.
+x = x(:, feat);
+
+% Shift and scale by mu and sigma.
 x=x-ones(n,1)*mu;
-%sig(~sig(:))=1;
 x=x*diag(1./sig);
 end
